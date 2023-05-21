ed0c632f77086b8ff20b4b75fd36c4f6bd50e4e8
ethan <8bitbanana.ec@gmail.com>
2023-05-21 13:07:26 +0100

    CSS modules

--- src/components/AttributeMenu.tsx
+++ src/components/AttributeMenu.tsx
@@ -1,8 +1,8 @@
 
-import "./AttributeMenu.css";
+import * as style from "./AttributeMenu.module.css"
 
 const AttributeMenu = () => (
-    <div className="menu">
+    <div className={style.menu}>
         <p>This is a test!</p>
     </div>
 );
