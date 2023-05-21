816a06fcdf6f19aad678fcceb96546c49ca22c7f
ethan <8bitbanana.ec@gmail.com>
2023-05-21 13:37:23 +0100

    AttributeMenu typescript component
    React components have a set props and state, I can let typescript know about this
    using the template arguments

--- src/components/AttributeMenu.tsx
+++ src/components/AttributeMenu.tsx
@@ -1,10 +1,32 @@
 
 import * as style from "./AttributeMenu.module.css"
 
-const AttributeMenu = () => (
-    <div className={style["menu-test"]}>
-        <p>This is a test!</p>
-    </div>
-);
+import { Attribute } from "../attribute";
+import { Component } from "preact";
+
+// Component<props, state>
+class AttributeMenuElement extends Component<{attrname: string}, {}> {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div>
+            <input type="text" value={this.props.attrname} />
+            <button>Eval</button>
+            <button>Delete</button>
+        </div>;
+    }
+}
+
+const AttributeMenu = () => {
+
+    const names = ["first test", "second test", "third test"];
+
+    return names.map((object, i) => {
+        return <AttributeMenuElement attrname={object}/>
+    });
+};
 
 export default AttributeMenu;
